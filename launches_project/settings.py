6ed03e98ce0ef438277883ca6930f2a9f7cb3ddb
trse2906 <trse2906@colorado.edu>
2024-06-02 19:07:50 -0600

    fixed way static files are handeled

--- launches_project/settings.py
+++ launches_project/settings.py
@@ -148,10 +148,6 @@ APPEND_SLASH = True
 STATIC_ROOT = BASE_DIR / "staticfiles"
 STATIC_URL = "/static/"
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "booster_tracker", "static"),
-]
-
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
